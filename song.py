1fb13f3365306428196911b9d75487ee56576216
Laura S <laurarodrsali@unisabana.edu.co>
2026-05-05 13:32:17 -0500

    2

--- song.py
+++ song.py
@@ -1,1 +1,16 @@
 class Song
+    def __init__(self, name, artist, length):
+        self.name = name
+        self.artist = artist
+        self.length = length
+ 
+    def get_length_in_seconds(self):
+        return self.length * 60
+ 
+    def __str__(self):
+        return f"'{self.name}' by {self.artist} ({self.length})"
+ 
+ 
+if __name__ == "__main__":
+    my_song = Song("tv off", "Kendrick Lamar", 3.7)
+    print(my_song.get_length_in_seconds()) 
